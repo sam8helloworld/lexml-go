9b602c5920882bfebadd6224bac84ac521b27619
sam8helloworld <samcreate0830@gmail.com>
2022-05-04 08:31:24 +0900

    test: dic-itemとheadタグをunmarshalする

---
+++ lexml_test.go
@@ -0,0 +1,30 @@
+package lexml
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/sam8helloworld/lexml-go/model"
+)
+
+func TestSuccessUnmarshalDicItem(t *testing.T) {
+	input := []byte(`
+  <dic-item id="sample01">
+  	<head></head>
+  </dic-item>`)
+	want := model.DicItem{
+		XMLName: xml.Name{Local: "dic-item"},
+		ID:      "sample01",
+		Head: model.Head{
+			XMLName: xml.Name{Local: "head"},
+		},
+	}
+	got, err := UnMarshal(input)
+	if err != nil {
+		t.Fatalf("failed to execute UnMarshal: %v", err)
+	}
+	if diff := cmp.Diff(*got, want); diff != "" {
+		t.Fatalf("got differs: (-got +want)\n%s", diff)
+	}
+}
