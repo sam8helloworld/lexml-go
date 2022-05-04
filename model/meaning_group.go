239a60b30c552d9d02755c31ff3dcb996c85d304
sam8helloworld <samcreate0830@gmail.com>
2022-05-04 11:12:53 +0900

    feat: meaning-groupタグのモデルを追加

---
+++ model/meaning_group.go
@@ -0,0 +1,57 @@
+package model
+
+import (
+	"encoding/xml"
+	"errors"
+)
+
+var ErrMeaningGroupChildrenUnknownElement = errors.New("meaning-group have unknown children")
+
+type MeaningGroup struct {
+	XMLName              xml.Name            `xml:"meaning-group"`
+	SubID                string              `xml:"subid,attr"`
+	Type                 string              `xml:"type,attr"`
+	Level                string              `xml:"level,attr"`
+	No                   string              `xml:"no,attr"`
+	MeaningGroupChildren []MeaningGroupChild `xml:",any"`
+}
+
+type MeaningGroupChild struct {
+	Type  string
+	Value interface{}
+}
+
+func (mgc *MeaningGroupChild) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	switch start.Name.Local {
+	case "meaning":
+		var m Meaning
+		if err := d.DecodeElement(&m, &start); err != nil {
+			return err
+		}
+		mgc.Value = m
+		mgc.Type = start.Name.Local
+	case "example":
+		var e Example
+		if err := d.DecodeElement(&e, &start); err != nil {
+			return err
+		}
+		mgc.Value = e
+		mgc.Type = start.Name.Local
+	case "subhead":
+		// TODO: subhead
+	case "column":
+		// TODO: column
+	case "key":
+		var k Key
+		if err := d.DecodeElement(&k, &start); err != nil {
+			return err
+		}
+		mgc.Value = k
+		mgc.Type = start.Name.Local
+	case "div":
+		// TODO: div
+	default:
+		return ErrHeadChildrenUnknownElement
+	}
+	return nil
+}
---
+++ model/meaning_group_test.go
@@ -0,0 +1,77 @@
+package model
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestMeaningGroupUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  MeaningGroupChild
+	}{
+		{
+			name:  "meaningをUnmarshalできる",
+			input: `<meaning>意味</meaning>`,
+			want: MeaningGroupChild{
+				Type: "meaning",
+				Value: Meaning{
+					XMLName: xml.Name{Local: "meaning"},
+					Value:   "意味",
+				},
+			},
+		},
+		{
+			name:  "exampleをUnmarshalできる",
+			input: `<example>例</example>`,
+			want: MeaningGroupChild{
+				Type: "example",
+				Value: Example{
+					XMLName: xml.Name{Local: "example"},
+					Value:   "例",
+				},
+			},
+		},
+		// TODO: subhead
+		// TODO: column
+		{
+			name:  "keyをUnmarshalできる",
+			input: `<key>key</key>`,
+			want: MeaningGroupChild{
+				Type: "key",
+				Value: Key{
+					XMLName: xml.Name{Local: "key"},
+					Value:   "key",
+				},
+			},
+		},
+		// TODO: div
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var got MeaningGroupChild
+			err := xml.Unmarshal([]byte(tt.input), &got)
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestMeaningGroupUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got HeadChild
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrHeadChildrenUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
