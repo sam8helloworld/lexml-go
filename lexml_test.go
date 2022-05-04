3c4d617b6e83f79650c57e76f915aa73e5601f69
sam8helloworld <samcreate0830@gmail.com>
2022-05-04 10:34:46 +0900

    test: 順不同のxmlの子要素をUnmarshalできる

--- lexml_test.go
+++ lexml_test.go
@@ -10,14 +10,32 @@ import (
 
 func TestSuccessUnmarshalDicItem(t *testing.T) {
 	input := []byte(`
-  <dic-item id="sample01">
-  	<head></head>
-  </dic-item>`)
+	<dic-item id="sample01">
+	<head>
+	<headword>plasma</headword>
+	<key>plasma</key>
+	<headword>pl&aeacute;zm&schwa;</headword>
+	</head>
+	</dic-item>`)
 	want := model.DicItem{
 		XMLName: xml.Name{Local: "dic-item"},
 		ID:      "sample01",
 		Head: model.Head{
 			XMLName: xml.Name{Local: "head"},
+			HeadChildren: []model.HeadChild{
+				{
+					Type:  "headword",
+					Value: model.Headword{XMLName: xml.Name{Local: "headword"}, Value: "plasma"},
+				},
+				{
+					Type:  "key",
+					Value: model.Key{XMLName: xml.Name{Local: "key"}, Value: "plasma"},
+				},
+				{
+					Type:  "headword",
+					Value: model.Headword{XMLName: xml.Name{Local: "headword"}, Value: "pl&aeacute;zm&schwa;"},
+				},
+			},
 		},
 	}
 	got, err := UnMarshal(input)
