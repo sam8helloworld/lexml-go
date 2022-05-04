c7aaef5cbaf4a4cbd2e8148c6926ae65954fa625
sam8helloworld <samcreate0830@gmail.com>
2022-05-04 10:55:33 +0900

    feat: meaningタグのモデルを追加

---
+++ model/meaning.go
@@ -0,0 +1,12 @@
+package model
+
+import "encoding/xml"
+
+type Meaning struct {
+	XMLName xml.Name `xml:"meaning"`
+	SubID   string   `xml:"subid,attr"`
+	Type    string   `xml:"type,attr"`
+	Level   string   `xml:"level,attr"`
+	No      string   `xml:"no,attr"`
+	Value   string   `xml:",innerxml"`
+}
