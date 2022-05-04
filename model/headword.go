2c29522939c54419474b101b2e23b32fd719996d
sam8helloworld <samcreate0830@gmail.com>
2022-05-04 10:49:58 +0900

    refactor: 予約語だと思ってたが大文字は関係なかった

--- model/headword.go
+++ model/headword.go
@@ -3,8 +3,8 @@ package model
 import "encoding/xml"
 
 type Headword struct {
-	XMLName      xml.Name `xml:"headword"`
-	HeadwordType string   `xml:"type,attr"`
-	Delimiter    string   `xml:"delimiter,attr"`
-	Value        string   `xml:",innerxml"`
+	XMLName   xml.Name `xml:"headword"`
+	Type      string   `xml:"type,attr"`
+	Delimiter string   `xml:"delimiter,attr"`
+	Value     string   `xml:",innerxml"`
 }
