8e29a921a0270fa0a009fb3730917df010b084cd
sam8helloworld <samcreate0830@gmail.com>
2022-05-06 18:03:57 +0900

    feat: d*系タグのモデルを追加

---
+++ model/structure/dd.go
@@ -0,0 +1,11 @@
+package model
+
+import "encoding/xml"
+
+type Dd struct {
+	Structure
+	XMLName xml.Name `xml:"dd"`
+	SubID   string   `xml:"subid,attr"`
+	Type    string   `xml:"type,attr"`
+	Value   string   `xml:",chardata"`
+}
