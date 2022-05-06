258a3b1940f95fe1d0cd18a3065a06a031960dda
sam8helloworld <samcreate0830@gmail.com>
2022-05-06 18:04:23 +0900

    feat: リスト系のタグのモデルを追加

---
+++ model/structure/li.go
@@ -0,0 +1,11 @@
+package model
+
+import "encoding/xml"
+
+type Li struct {
+	Structure
+	XMLName xml.Name `xml:"li"`
+	SubID   string   `xml:"subid,attr"`
+	Type    string   `xml:"type,attr"`
+	Value   string   `xml:",chardata"`
+}
