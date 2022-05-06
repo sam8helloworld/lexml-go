6396caa740d742eb02d445e8e6e9cdbb1a0b80c5
sam8helloworld <samcreate0830@gmail.com>
2022-05-06 18:03:29 +0900

    feat: pタグのモデルを追加

---
+++ model/structure/p.go
@@ -0,0 +1,11 @@
+package model
+
+import "encoding/xml"
+
+type P struct {
+	Structure
+	XMLName xml.Name `xml:"p"`
+	SubID   string   `xml:"subid,attr"`
+	Type    string   `xml:"type,attr"`
+	Value   string   `xml:",chardata"`
+}
