5fbc97815b07b0334f7f7b9ba4ff88308d50726e
sam8helloworld <samcreate0830@gmail.com>
2022-05-06 20:04:47 +0900

    feat: label系タグのモデルを追加

---
+++ model/inline_lexml/slabel.go
@@ -0,0 +1,12 @@
+package model
+
+import "encoding/xml"
+
+type SLabel struct {
+	InlineLeXML
+	XMLName xml.Name `xml:"slabel"`
+	Type    string   `xml:"type,attr"`
+	Genre   string   `xml:"genre,attr"`
+	Code    string   `xml:"code,attr"`
+	Value   string   `xml:",chardata"`
+}
