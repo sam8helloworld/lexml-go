9f18b5f2a3b1aa79282c86e76f5cbc24d01cf3e3
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:23:45 +0900

    feat: klokuriエンティティでインライン子要素を追加

---
+++ entity/inline_lexml_klokuri_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestKlOkuriUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "klokuri内のinline_htmlをUnmarshalできる",
+			input: `<klokuri><b>b</b></klokuri>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "klokuri内のinline_lexmlをUnmarshalできる",
+			input: `<klokuri><alabel>alabel</alabel></klokuri>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "klokuri内のpcdataをUnmarshalできる",
+			input: `<klokuri>pcdata</klokuri>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "klokuri内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<klokuri><b>b</b><alabel>alabel</alabel>pcdata</klokuri>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var k KlOkuri
+			err := xml.Unmarshal([]byte(tt.input), &k)
+			got := k.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestKlOkuriUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got KlOkuri
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrKlOkuriUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
