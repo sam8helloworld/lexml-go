d777dc57d8050d6485b92edb82d45613f1216d56
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:56:03 +0900

    feat: provnエンティティでインライン子要素を追加

--- entity/inline_lexml_pro_v.go
+++ entity/inline_lexml_pro_v.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrProVUnknownElement = errors.New("pro-v have unknown children")
 
 type ProV struct {
 	InlineLeXML
-	XMLName xml.Name `xml:"pron-v"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	XMLName xml.Name `xml:"pro-v"`
+	Value   InnerXML
+}
+
+func (p *ProV) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var pp struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"pro-v"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&pp, &start); err != nil {
+		return errors.Wrap(ErrProVUnknownElement, "failed to unmarshal data")
+	}
+	*p = ProV{
+		XMLName: xml.Name{Local: "pro-v"},
+		Value: InnerXML{
+			Value: pp.Value,
+		},
+	}
+	return nil
 }
