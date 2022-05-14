314e59ef8fdee3c02f523f956d712b4670a3a159
sam8helloworld <samcreate0830@gmail.com>
2022-05-14 23:17:16 +0900

    feat: ex-transエンティティでインライン子要素を追加

--- entity/inline_lexml_ex_trans.go
+++ entity/inline_lexml_ex_trans.go
@@ -1,11 +1,39 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrExTransUnknownElement = errors.New("ex-trans have unknown children")
 
 type ExTrans struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"ex-trans"`
 	Lang    string   `xml:"lang,attr"`
 	Type    string   `xml:"type,attr"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (e *ExTrans) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ee struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"ex-trans"`
+		Lang    string   `xml:"lang,attr"`
+		Type    string   `xml:"type,attr"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ee, &start); err != nil {
+		return errors.Wrap(ErrExTransUnknownElement, "failed to unmarshal data")
+	}
+	*e = ExTrans{
+		XMLName: xml.Name{Local: "ex-trans"},
+		Lang:    ee.Lang,
+		Type:    ee.Type,
+		Value: InnerXML{
+			Value: ee.Value,
+		},
+	}
+	return nil
 }
