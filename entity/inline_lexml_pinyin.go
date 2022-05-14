475158e9943ac32eda5e1cfcb220e40fa1499916
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:47:43 +0900

    feat: pinyinエンティティでインライン子要素を追加

--- entity/inline_lexml_pinyin.go
+++ entity/inline_lexml_pinyin.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrPinyinUnknownElement = errors.New("pinyin have unknown children")
 
 type Pinyin struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"pinyin"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (p *Pinyin) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var pp struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"pinyin"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&pp, &start); err != nil {
+		return errors.Wrap(ErrPinyinUnknownElement, "failed to unmarshal data")
+	}
+	*p = Pinyin{
+		XMLName: xml.Name{Local: "pinyin"},
+		Value: InnerXML{
+			Value: pp.Value,
+		},
+	}
+	return nil
 }
---
+++ entity/inline_lexml_pinyin_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestPinyinUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "pinyin内のinline_htmlをUnmarshalできる",
+			input: `<pinyin><b>b</b></pinyin>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "pinyin内のinline_lexmlをUnmarshalできる",
+			input: `<pinyin><alabel>alabel</alabel></pinyin>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "pinyin内のpcdataをUnmarshalできる",
+			input: `<pinyin>pcdata</pinyin>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "pinyin内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<pinyin><b>b</b><alabel>alabel</alabel>pcdata</pinyin>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var p Pinyin
+			err := xml.Unmarshal([]byte(tt.input), &p)
+			got := p.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestPinyinUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got Pinyin
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrPinyinUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
