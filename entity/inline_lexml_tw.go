91259d69c8fe1a50389b160ca1b958e90145420c
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 01:08:07 +0900

    feat: twエンティティでインライン子要素を追加

--- entity/inline_lexml_tw.go
+++ entity/inline_lexml_tw.go
@@ -1,9 +1,34 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrTwUnknownElement = errors.New("tw have unknown children")
 
 type Tw struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"tw"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (t *Tw) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var tt struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"tw"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&tt, &start); err != nil {
+		return errors.Wrap(ErrTwUnknownElement, "failed to unmarshal data")
+	}
+	*t = Tw{
+		XMLName: xml.Name{Local: "tw"},
+
+		Value: InnerXML{
+			Value: tt.Value,
+		},
+	}
+	return nil
 }
---
+++ entity/inline_lexml_tw_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestTwUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "tw内のinline_htmlをUnmarshalできる",
+			input: `<tw><b>b</b></tw>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "tw内のinline_lexmlをUnmarshalできる",
+			input: `<tw><alabel>alabel</alabel></tw>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "tw内のpcdataをUnmarshalできる",
+			input: `<tw>pcdata</tw>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "tw内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<tw><b>b</b><alabel>alabel</alabel>pcdata</tw>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var tw Tw
+			err := xml.Unmarshal([]byte(tt.input), &tw)
+			got := tw.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestTwUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got Tw
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrTwUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
