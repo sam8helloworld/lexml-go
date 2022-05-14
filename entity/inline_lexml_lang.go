838f5f05dc5cfa9f222bac64a2b3c85b80584bc5
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:35:11 +0900

    feat: langエンティティでインライン子要素を追加

--- entity/inline_lexml_lang.go
+++ entity/inline_lexml_lang.go
@@ -1,10 +1,36 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrLangUnknownElement = errors.New("kigo have unknown children")
 
 type Lang struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"lang"`
 	Type    string   `xml:"type,attr"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (l *Lang) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ll struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"lang"`
+		Type    string   `xml:"type,attr"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ll, &start); err != nil {
+		return errors.Wrap(ErrLangUnknownElement, "failed to unmarshal data")
+	}
+	*l = Lang{
+		XMLName: xml.Name{Local: "lang"},
+		Type:    ll.Type,
+		Value: InnerXML{
+			Value: ll.Value,
+		},
+	}
+	return nil
 }
