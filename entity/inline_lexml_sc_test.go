baef074276cc88d9a51c4135e87a8421f3b76f49
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:59:09 +0900

    feat: scエンティティでインライン子要素を追加

---
+++ entity/inline_lexml_sc_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestScUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "sc内のinline_htmlをUnmarshalできる",
+			input: `<sc><b>b</b></sc>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "sc内のinline_lexmlをUnmarshalできる",
+			input: `<sc><alabel>alabel</alabel></sc>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "sc内のpcdataをUnmarshalできる",
+			input: `<sc>pcdata</sc>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "sc内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<sc><b>b</b><alabel>alabel</alabel>pcdata</sc>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var s Sc
+			err := xml.Unmarshal([]byte(tt.input), &s)
+			got := s.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestScUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got Sc
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrScUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
