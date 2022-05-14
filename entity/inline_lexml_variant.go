507bd25d47017958eb45e24d3b0e43ac825753f8
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 01:15:36 +0900

    feat: varintエンティティでインライン子要素を追加

--- entity/inline_lexml_variant.go
+++ entity/inline_lexml_variant.go
@@ -1,10 +1,36 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrVariantUnknownElement = errors.New("variant have unknown children")
 
 type Variant struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"variant"`
 	Type    string   `xml:"type,attr"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (v *Variant) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var vv struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"variant"`
+		Type    string   `xml:"type,attr"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&vv, &start); err != nil {
+		return errors.Wrap(ErrVariantUnknownElement, "failed to unmarshal data")
+	}
+	*v = Variant{
+		XMLName: xml.Name{Local: "variant"},
+		Type:    vv.Type,
+		Value: InnerXML{
+			Value: vv.Value,
+		},
+	}
+	return nil
 }
