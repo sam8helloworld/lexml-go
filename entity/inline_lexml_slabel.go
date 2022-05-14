e0b571e41a279ce546fb4d8eff9baa411e85dd39
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 01:03:03 +0900

    feat: slabelエンティティでインライン子要素を追加

--- entity/inline_lexml_slabel.go
+++ entity/inline_lexml_slabel.go
@@ -1,6 +1,12 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrSLabelUnknownElement = errors.New("slabel have unknown children")
 
 type SLabel struct {
 	InlineLeXML
@@ -8,5 +14,29 @@ type SLabel struct {
 	Type    string   `xml:"type,attr"`
 	Genre   string   `xml:"genre,attr"`
 	Code    string   `xml:"code,attr"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (s *SLabel) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ss struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"slabel"`
+		Type    string   `xml:"type,attr"`
+		Genre   string   `xml:"genre,attr"`
+		Code    string   `xml:"code,attr"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ss, &start); err != nil {
+		return errors.Wrap(ErrSLabelUnknownElement, "failed to unmarshal data")
+	}
+	*s = SLabel{
+		XMLName: xml.Name{Local: "slabel"},
+		Type:    ss.Type,
+		Genre:   ss.Genre,
+		Code:    ss.Code,
+		Value: InnerXML{
+			Value: ss.Value,
+		},
+	}
+	return nil
 }
