e3717f2ba8cfd002c8a53929b9eaaa2362a765fd
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:32:48 +0900

    feat: krtエンティティでインライン子要素を追加

--- entity/inline_lexml_krt.go
+++ entity/inline_lexml_krt.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrKRtUnknownElement = errors.New("krt have unknown children")
 
 type KRt struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"krt"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (k *KRt) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var kk struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"krt"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&kk, &start); err != nil {
+		return errors.Wrap(ErrKRtUnknownElement, "failed to unmarshal data")
+	}
+	*k = KRt{
+		XMLName: xml.Name{Local: "krt"},
+		Value: InnerXML{
+			Value: kk.Value,
+		},
+	}
+	return nil
 }
