c7361770597a49b482cddb68ba490422bce336a1
sam8helloworld <samcreate0830@gmail.com>
2022-05-14 16:02:29 +0900

    feat: inner要素の構造体をheadwordに適用

--- model/structure/headword.go
+++ model/structure/headword.go
@@ -2,12 +2,39 @@ package structure
 
 import (
 	"encoding/xml"
+
+	"github.com/pkg/errors"
+	"github.com/sam8helloworld/lexml-go/model"
 )
+
+var ErrHeadwordUnknownElement = errors.New("headword have unknown children")
 
 type Headword struct {
 	Structure
 	XMLName   xml.Name `xml:"headword"`
 	Type      string   `xml:"type,attr"`
 	Delimiter string   `xml:"delimiter,attr"`
-	Value     string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value     model.InnerXML
+}
+
+func (h *Headword) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var hh struct {
+		Structure
+		XMLName   xml.Name `xml:"headword"`
+		Type      string   `xml:"type,attr"`
+		Delimiter string   `xml:"delimiter,attr"`
+		Value     string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&hh, &start); err != nil {
+		return errors.Wrap(ErrHeadwordUnknownElement, "failed to unmarshal data")
+	}
+	*h = Headword{
+		XMLName:   xml.Name{Local: "headword"},
+		Type:      hh.Type,
+		Delimiter: hh.Delimiter,
+		Value: model.InnerXML{
+			Value: hh.Value,
+		},
+	}
+	return nil
 }
