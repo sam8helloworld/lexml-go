0bae485528b291459d15f97391c43ce67d1ff904
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:42:30 +0900

    feat: oblqエンティティでインライン子要素を追加

--- entity/inline_lexml_oblq.go
+++ entity/inline_lexml_oblq.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrOblqUnknownElement = errors.New("oblq have unknown children")
 
 type Oblq struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"oblq"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (o *Oblq) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var oo struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"oblq"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&oo, &start); err != nil {
+		return errors.Wrap(ErrOblqUnknownElement, "failed to unmarshal data")
+	}
+	*o = Oblq{
+		XMLName: xml.Name{Local: "oblq"},
+		Value: InnerXML{
+			Value: oo.Value,
+		},
+	}
+	return nil
 }
