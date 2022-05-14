58dfd43ff1cbb5095ca5f7a4768fd4755acb4c37
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:16:06 +0900

    feat: jpエンティティでインライン子要素を追加

--- entity/inline_lexml_jp.go
+++ entity/inline_lexml_jp.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrJpUnknownElement = errors.New("jp have unknown children")
 
 type Jp struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"jp"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (j *Jp) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var jj struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"jp"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&jj, &start); err != nil {
+		return errors.Wrap(ErrJpUnknownElement, "failed to unmarshal data")
+	}
+	*j = Jp{
+		XMLName: xml.Name{Local: "jp"},
+		Value: InnerXML{
+			Value: jj.Value,
+		},
+	}
+	return nil
 }
