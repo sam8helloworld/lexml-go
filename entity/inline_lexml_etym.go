e6df0686bfc24c5307f790a79b334acf6dfadd4a
sam8helloworld <samcreate0830@gmail.com>
2022-05-14 23:00:41 +0900

    feat: etymエンティティでインライン子要素を追加

--- entity/inline_lexml_etym.go
+++ entity/inline_lexml_etym.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrEtymUnknownElement = errors.New("etym have unknown children")
 
 type Etym struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"etym"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (e *Etym) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ee struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"etym"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ee, &start); err != nil {
+		return errors.Wrap(ErrEtymUnknownElement, "failed to unmarshal data")
+	}
+	*e = Etym{
+		XMLName: xml.Name{Local: "etym"},
+		Value: InnerXML{
+			Value: ee.Value,
+		},
+	}
+	return nil
 }
