a4a1d4f51f51f836d0147818ecbc6de9e144c9ee
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:37:20 +0900

    feat: lightエンティティでインライン子要素を追加

--- entity/inline_lexml_light.go
+++ entity/inline_lexml_light.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrLightUnknownElement = errors.New("light have unknown children")
 
 type Light struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"light"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (l *Light) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ll struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"light"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ll, &start); err != nil {
+		return errors.Wrap(ErrLightUnknownElement, "failed to unmarshal data")
+	}
+	*l = Light{
+		XMLName: xml.Name{Local: "light"},
+		Value: InnerXML{
+			Value: ll.Value,
+		},
+	}
+	return nil
 }
