611ddd810b9ef13ee60bb4192b2553dd7ff4fe49
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:13:38 +0900

    feat: ipaエンティティでインライン子要素を追加

--- entity/inline_lexml_ipa.go
+++ entity/inline_lexml_ipa.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrIpaUnknownElement = errors.New("ipa have unknown children")
 
 type IPA struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"ipa"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (i *IPA) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ii struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"ipa"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ii, &start); err != nil {
+		return errors.Wrap(ErrIpaUnknownElement, "failed to unmarshal data")
+	}
+	*i = IPA{
+		XMLName: xml.Name{Local: "ipa"},
+		Value: InnerXML{
+			Value: ii.Value,
+		},
+	}
+	return nil
 }
