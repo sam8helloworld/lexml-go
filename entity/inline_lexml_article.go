8f7c88528f9c58cee02ed2367ca761db6e68d56f
sam8helloworld <samcreate0830@gmail.com>
2022-05-14 22:53:00 +0900

    feat: articleエンティティでインライン子要素を追加

--- entity/inline_lexml_article.go
+++ entity/inline_lexml_article.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrArticleUnknownElement = errors.New("article have unknown children")
 
 type Article struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"article"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (a *Article) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var aa struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"article"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&aa, &start); err != nil {
+		return errors.Wrap(ErrArticleUnknownElement, "failed to unmarshal data")
+	}
+	*a = Article{
+		XMLName: xml.Name{Local: "article"},
+		Value: InnerXML{
+			Value: aa.Value,
+		},
+	}
+	return nil
 }
