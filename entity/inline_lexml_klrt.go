8fbb443c40248521ccb7c9295f0a8e00adc856e7
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:26:13 +0900

    feat: klrtエンティティでインライン子要素を追加

--- entity/inline_lexml_klrt.go
+++ entity/inline_lexml_klrt.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrKlRtUnknownElement = errors.New("klrt have unknown children")
 
 type KlRt struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"klrt"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (k *KlRt) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var kk struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"klrt"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&kk, &start); err != nil {
+		return errors.Wrap(ErrKlRtUnknownElement, "failed to unmarshal data")
+	}
+	*k = KlRt{
+		XMLName: xml.Name{Local: "klrt"},
+		Value: InnerXML{
+			Value: kk.Value,
+		},
+	}
+	return nil
 }
---
+++ entity/inline_lexml_klrt_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestKlRtUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "klrt内のinline_htmlをUnmarshalできる",
+			input: `<klrt><b>b</b></klrt>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "klrt内のinline_lexmlをUnmarshalできる",
+			input: `<klrt><alabel>alabel</alabel></klrt>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "klrt内のpcdataをUnmarshalできる",
+			input: `<klrt>pcdata</klrt>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "klrt内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<klrt><b>b</b><alabel>alabel</alabel>pcdata</klrt>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var k KlRt
+			err := xml.Unmarshal([]byte(tt.input), &k)
+			got := k.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestKlRtUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got KlRt
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrKlRtUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
