4bb8fc9115129b665a91886700d8553bb08cee5d
sam8helloworld <samcreate0830@gmail.com>
2022-05-14 23:41:31 +0900

    feat: hiddenエンティティでインライン子要素を追加

--- entity/inline_lexml_hidden.go
+++ entity/inline_lexml_hidden.go
@@ -1,11 +1,39 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrHiddenUnknownElement = errors.New("hidden have unknown children")
 
 type Hidden struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"hidden"`
 	Type    string   `xml:"type,attr"`
 	Memo    string   `xml:"memo,attr"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (h *Hidden) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var hh struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"hidden"`
+		Type    string   `xml:"type,attr"`
+		Memo    string   `xml:"memo,attr"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&hh, &start); err != nil {
+		return errors.Wrap(ErrHiddenUnknownElement, "failed to unmarshal data")
+	}
+	*h = Hidden{
+		XMLName: xml.Name{Local: "hidden"},
+		Type:    hh.Type,
+		Memo:    hh.Memo,
+		Value: InnerXML{
+			Value: hh.Value,
+		},
+	}
+	return nil
 }
---
+++ entity/inline_lexml_hidden_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestHiddenUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "hidden内のinline_htmlをUnmarshalできる",
+			input: `<hidden><b>b</b></hidden>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "hidden内のinline_lexmlをUnmarshalできる",
+			input: `<hidden><alabel>alabel</alabel></hidden>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "hidden内のpcdataをUnmarshalできる",
+			input: `<hidden>pcdata</hidden>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "hidden内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<hidden><b>b</b><alabel>alabel</alabel>pcdata</hidden>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var h Hidden
+			err := xml.Unmarshal([]byte(tt.input), &h)
+			got := h.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestHiddenUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got Hidden
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrHiddenUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
