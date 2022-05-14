908bd955255717058f28c348e428bc1334e687b0
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 00:50:06 +0900

    feat: primaryエンティティでインライン子要素を追加

--- entity/inline_lexml_primary.go
+++ entity/inline_lexml_primary.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrPrimaryUnknownElement = errors.New("primary have unknown children")
 
 type Primary struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"primary"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (p *Primary) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var pp struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"primary"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&pp, &start); err != nil {
+		return errors.Wrap(ErrPrimaryUnknownElement, "failed to unmarshal data")
+	}
+	*p = Primary{
+		XMLName: xml.Name{Local: "primary"},
+		Value: InnerXML{
+			Value: pp.Value,
+		},
+	}
+	return nil
 }
