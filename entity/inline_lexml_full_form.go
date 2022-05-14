028d09a0d590753b9cae89e9b2155316ec9de1f0
sam8helloworld <samcreate0830@gmail.com>
2022-05-14 23:31:33 +0900

    feat: fullformエンティティでインライン子要素を追加

--- entity/inline_lexml_full_form.go
+++ entity/inline_lexml_full_form.go
@@ -1,9 +1,33 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrFullFormUnknownElement = errors.New("fullform have unknown children")
 
 type FullForm struct {
 	InlineLeXML
 	XMLName xml.Name `xml:"fullform"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (f *FullForm) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ff struct {
+		InlineLeXML
+		XMLName xml.Name `xml:"fullform"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ff, &start); err != nil {
+		return errors.Wrap(ErrFullFormUnknownElement, "failed to unmarshal data")
+	}
+	*f = FullForm{
+		XMLName: xml.Name{Local: "fullform"},
+		Value: InnerXML{
+			Value: ff.Value,
+		},
+	}
+	return nil
 }
---
+++ entity/inline_lexml_full_form_test.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"encoding/xml"
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFullFormUnmarshalXML_Success(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []Entity
+	}{
+		{
+			name:  "fullform内のinline_htmlをUnmarshalできる",
+			input: `<fullform><b>b</b></fullform>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+			},
+		},
+		{
+			name:  "fullform内のinline_lexmlをUnmarshalできる",
+			input: `<fullform><alabel>alabel</alabel></fullform>`,
+			want: []Entity{
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:  "fullform内のpcdataをUnmarshalできる",
+			input: `<fullform>pcdata</fullform>`,
+			want: []Entity{
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+		{
+			name:  "fullform内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<fullform><b>b</b><alabel>alabel</alabel>pcdata</fullform>`,
+			want: []Entity{
+				{
+					Type: "b",
+					Value: SmallB{
+						XMLName: xml.Name{Local: "b"},
+						Value:   "b",
+					},
+				},
+				{
+					Type: "alabel",
+					Value: ALabel{
+						XMLName: xml.Name{Local: "alabel"},
+						Value: InnerXML{
+							Value: "alabel",
+						},
+					},
+				},
+				{
+					Type: "pcdata",
+					Value: PCDATA{
+						Value: "pcdata",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			var f FullForm
+			err := xml.Unmarshal([]byte(tt.input), &f)
+			got := f.Value.StructuredValue()
+			if err != nil {
+				t.Fatalf("failed to execute Unmarshal %#v", err)
+			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Fatalf("got differs: (-got +want)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestFullFormUnmarshalXML_Fail(t *testing.T) {
+	input := []byte(`<unknown>plasma</unknown>`)
+	var got FullForm
+	err := xml.Unmarshal(input, &got)
+	if !errors.Is(err, ErrFullFormUnknownElement) {
+		t.Fatalf("failed to execute Unmarshal %#v", err)
+	}
+}
