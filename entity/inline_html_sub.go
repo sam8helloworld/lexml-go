6a0bc3a0bd738952c25ca58a5d8717ee5289490a
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 20:01:27 +0900

    feat: subエンティティでインライン子要素を追加

--- entity/inline_html_sub.go
+++ entity/inline_html_sub.go
@@ -1,15 +1,57 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrSubUnknownElement = errors.New("sub have unknown children")
 
 type SmallSub struct {
 	InlineHTML
 	XMLName xml.Name `xml:"sub"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
 }
 
 type LargeSub struct {
 	InlineHTML
 	XMLName xml.Name `xml:"SUB"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (s *SmallSub) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ss struct {
+		InlineHTML
+		XMLName xml.Name `xml:"sub"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ss, &start); err != nil {
+		return errors.Wrap(ErrSubUnknownElement, "failed to unmarshal data")
+	}
+	*s = SmallSub{
+		XMLName: xml.Name{Local: "sub"},
+		Value: InnerXML{
+			Value: ss.Value,
+		},
+	}
+	return nil
+}
+
+func (s *LargeSub) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var ss struct {
+		InlineHTML
+		XMLName xml.Name `xml:"SUB"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&ss, &start); err != nil {
+		return errors.Wrap(ErrSubUnknownElement, "failed to unmarshal data")
+	}
+	*s = LargeSub{
+		XMLName: xml.Name{Local: "SUB"},
+		Value: InnerXML{
+			Value: ss.Value,
+		},
+	}
+	return nil
 }
