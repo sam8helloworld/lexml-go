4bba22c0a517f974dc6b0aeb9a61de6336457e2b
sam8helloworld <samcreate0830@gmail.com>
2022-05-15 18:27:19 +0900

    feat: bigエンティティでインライン子要素を追加

--- entity/inline_html_big.go
+++ entity/inline_html_big.go
@@ -1,11 +1,39 @@
 package entity
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+
+	"github.com/pkg/errors"
+)
+
+var ErrBigUnknownElement = errors.New("big have unknown children")
 
 type Big struct {
 	InlineHTML
 	XMLName xml.Name `xml:"big"`
 	Type    string   `xml:"type,attr"`
 	Class   string   `xml:"class,attr"`
-	Value   string   `xml:",chardata"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	Value   InnerXML
+}
+
+func (b *Big) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var bb struct {
+		InlineHTML
+		XMLName xml.Name `xml:"big"`
+		Type    string   `xml:"type,attr"`
+		Class   string   `xml:"class,attr"`
+		Value   string   `xml:",innerxml"` // (#PCDATA | %inline.html; | %inline.lexml;)*
+	}
+	if err := d.DecodeElement(&bb, &start); err != nil {
+		return errors.Wrap(ErrBigUnknownElement, "failed to unmarshal data")
+	}
+	*b = Big{
+		XMLName: xml.Name{Local: "big"},
+		Type:    bb.Type,
+		Class:   bb.Class,
+		Value: InnerXML{
+			Value: bb.Value,
+		},
+	}
+	return nil
 }
--- entity/inline_html_big_test.go
+++ entity/inline_html_big_test.go
@@ -8,15 +8,15 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestAUnmarshalXML_Success(t *testing.T) {
+func TestBigUnmarshalXML_Success(t *testing.T) {
 	tests := []struct {
 		name  string
 		input string
 		want  []Entity
 	}{
 		{
-			name:  "a内のinline_htmlをUnmarshalできる",
-			input: `<a><b>b</b></a>`,
+			name:  "big内のinline_htmlをUnmarshalできる",
+			input: `<big><b>b</b></big>`,
 			want: []Entity{
 				{
 					Type: "b",
@@ -28,8 +28,8 @@ func TestAUnmarshalXML_Success(t *testing.T) {
 			},
 		},
 		{
-			name:  "a内のinline_lexmlをUnmarshalできる",
-			input: `<a><alabel>alabel</alabel></a>`,
+			name:  "big内のinline_lexmlをUnmarshalできる",
+			input: `<big><alabel>alabel</alabel></big>`,
 			want: []Entity{
 				{
 					Type: "alabel",
@@ -43,8 +43,8 @@ func TestAUnmarshalXML_Success(t *testing.T) {
 			},
 		},
 		{
-			name:  "a内のpcdataをUnmarshalできる",
-			input: `<a>pcdata</a>`,
+			name:  "big内のpcdataをUnmarshalできる",
+			input: `<big>pcdata</big>`,
 			want: []Entity{
 				{
 					Type: "pcdata",
@@ -55,8 +55,8 @@ func TestAUnmarshalXML_Success(t *testing.T) {
 			},
 		},
 		{
-			name:  "a内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
-			input: `<a><b>b</b><alabel>alabel</alabel>pcdata</a>`,
+			name:  "big内のinline_html, inline_lexml, pcdataを同時にUnmarshalできる",
+			input: `<big><b>b</b><alabel>alabel</alabel>pcdata</big>`,
 			want: []Entity{
 				{
 					Type: "b",
@@ -87,9 +87,9 @@ func TestAUnmarshalXML_Success(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			var a A
-			err := xml.Unmarshal([]byte(tt.input), &a)
-			got := a.Value.StructuredValue()
+			var b Big
+			err := xml.Unmarshal([]byte(tt.input), &b)
+			got := b.Value.StructuredValue()
 			if err != nil {
 				t.Fatalf("failed to execute Unmarshal %#v", err)
 			}
@@ -100,11 +100,11 @@ func TestAUnmarshalXML_Success(t *testing.T) {
 	}
 }
 
-func TestAUnmarshalXML_Fail(t *testing.T) {
+func TestBigUnmarshalXML_Fail(t *testing.T) {
 	input := []byte(`<unknown>plasma</unknown>`)
-	var got A
+	var got Big
 	err := xml.Unmarshal(input, &got)
-	if !errors.Is(err, ErrAUnknownElement) {
+	if !errors.Is(err, ErrBigUnknownElement) {
 		t.Fatalf("failed to execute Unmarshal %#v", err)
 	}
 }
